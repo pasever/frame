63feb0e7501806d917ee8942e0e6f8ca361a3e5d
Evgheni Pascalov <evgheni@Evghenis-MBP.attlocal.net>
2019-11-29 16:34:20 -0500

    working on the scopes

---
+++ src/scope.js
@@ -0,0 +1,32 @@
+/* jshint glabalstrict: true */
+
+'use strict';
+
+function Scope() {
+
+    this.$$watchers = [];
+
+    Scope.prototype.$watch = function(watchFn, listenerFn) {
+        var watcher = {
+          watchFn: watchFn,
+          listenerFn: listenerFn
+        };
+        this.$$watchers.push(watcher);
+    };
+
+    Scope.prototype.$digest = function () {
+        var self = this;
+        var newValue, oldValue;
+
+      _.forEach(this.$$watchers, function(watcher) {
+         newValue = watcher.watchFn(self);
+         oldValue = watcher.last;
+         // watcher.watchFn(self);
+         // watcher.listenerFn();
+          if (newValue !== oldValue) {
+              watcher.last = newValue;
+              watcher.listenerFn(newValue, oldValue, self);
+          }
+      });
+    };
+}
---
+++ test/scope_spec.js
@@ -0,0 +1,85 @@
+'use strict';
+/* jshint glabalstrict: true */
+/* glabal Scope: false */
+
+describe("Scope", function() {
+    it("can be contructed and used as an object", function() {
+       var scope = new Scope;
+       scope.aProperty = 1;
+
+       expect(scope.aProperty).toBe(1);
+    });
+});
+
+
+decribe("digest", function() {
+   var scope;
+
+   beforeEach(function() {
+      scope = new Scope();
+   });
+
+   it("calls the listener function of a watch on first $digest", function() {
+      var watchFn = function() { return "wat"; };
+      var listenerFn = jasmine.createSpy();
+      scope.$watch(watchFn, listenerFn);
+
+      scope.$digest();
+
+      expect(listenerFn).toHaveBeenCalled();
+   });
+
+   it("calls the watch function with the scope as the argument", function() {
+      var watchFn = jasmine.createSpy();
+      var listenerFn = function () {
+
+      };
+
+      scope.$watch(watchFn, listenerFn);
+      scope.$digest();
+
+      expect(watchFn).toHaveBeenCalledWith(scope);
+   });
+
+   it("calls the listener function when the watched value changes", function() {
+      scope.someValue = 'a';
+      scope.counter = 0;
+
+      scope.$watch(
+          function(scope) { return scope.someValue; },
+          function(newValue, oldValue, scope) { scope.counter++; }
+      );
+
+      expect(scope.counter).toBe(0);
+
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+
+
+      scope.someValue = 'b';
+      expect(scope.counter).toBe(1);
+
+      scope.$digest();
+      expect(scope.counter).toBe(2);
+
+   });
+
+   it("calls listener when watch value is first undefined", function() {
+      scope.counter = 0;
+
+      scope.$watch(
+          function (scope) {
+             return scope.someValue;
+          },
+          function (newValue, oldValue, scope) {
+              scope.counter++;
+          }
+      );
+
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+   });
+});
